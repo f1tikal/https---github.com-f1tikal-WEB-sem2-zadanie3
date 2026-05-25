6712c654a50a2c0a26781bc4eb11d947ea13ec3d
f1tikal <maksimvin06@gmail.com>
2026-05-25 21:11:24 +0300

    new test

--- test.php
+++ test.php
@@ -1,3 +1,8 @@
 <?php
-phpinfo();
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=u82089', 'u82089', '4044723');
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
